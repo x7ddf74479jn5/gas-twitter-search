e5065e4690c3b70b6b22f61d7a83eff74b76c530
Pandashark <68179184+x7ddf74479jn5@users.noreply.github.com>
2022-11-15 01:38:06 +0900

    🔥 remove

--- src/utils/env.ts
+++
@@ -1,5 +0,0 @@
-export const getEnv = () => {
-  return {
-    SLACK_WEBHOOK_URL: process.env.SLACK_WEBHOOK_URL,
-  };
-};
--- src/utils/format.ts
+++
@@ -1,1 +0,0 @@
-export const formatDate = (date: Date) => Utilities.formatDate(date, "Asia/Tokyo", "yyyy/MM/dd HH:mm:ss");
